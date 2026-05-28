a7c20a40cd636419cf4db1605d20574af7135dde
spinor <spinor@spinor.com>
2026-05-28 16:07:58 +0530

    chore(config): proxy /docs route to dev server

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,10 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
+      '/docs': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
     },
   },
 });
